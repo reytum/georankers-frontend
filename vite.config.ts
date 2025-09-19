2d2aa6fc487daa8f8ab735cbaede38160bbd48c0
Gaurav Bhatt <97746934+gbhatt7@users.noreply.github.com>
2025-09-19 14:08:32 +0530

    Revert "netlify proxy changes"
    This reverts commit bc9506bd1388d924ae6271b49de377fb090a367d.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
-    proxy: {
-      "/api": {
-        target: "http://34.131.146.38:8080",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
